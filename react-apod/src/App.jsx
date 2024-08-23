b6ab202a033866f448fc746336ee89deee938e52
Samir Kharel 🧑🏽‍💻 <151682814+saamirkhrl@users.noreply.github.com>
2024-08-23 16:54:15 -0500

    Initial commit

---
+++ react-apod/src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [apiData, setApiData] = useState(null);
+
+  useEffect(() => {
+    function getApiData() {
+      fetch(
+        "https://api.nasa.gov/planetary/apod?api_key=4wVCH5RSiHB0SOEPAlywj0Pbb7hyHS8E7WNO0ro3"
+      )
+        .then((response) => response.json())
+        .then((data) => setApiData(data))
+        .catch((error) => console.error("Error fetching data:", error));
+    }
+
+    getApiData();
+  }, []);
+
+  if (!apiData) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <div className="container">
+        <h1>{apiData.title}</h1>
+        <img src={apiData.url} alt={apiData.title} />
+        <p>{apiData.description}</p>
+      </div>
+    );
+  }
+}
+
+export default App;
